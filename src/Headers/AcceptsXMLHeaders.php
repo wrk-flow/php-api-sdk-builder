e4f960af6d64cdc5df6722b37f5b8ee26eaeb64d
Martin Kluska <martin@kluska.cz>
2022-05-10 17:36:27 +0200

    Add AccetptsXMLHeaders

---
+++ src/Headers/AcceptsXMLHeaders.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WrkFlow\ApiSdkBuilder\Headers;
+
+use WrkFlow\ApiSdkBuilder\Contracts\HeadersContract;
+
+class AcceptsXMLHeaders implements HeadersContract
+{
+    public function headers(): array
+    {
+        return [
+            'Content-type' => 'application/xml',
+        ];
+    }
+}
