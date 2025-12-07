2b667e3aec9323580e0b6b8b56b4de6bf3c29090
Martin Kluska <martin@kluska.cz>
2025-12-07 17:42:13 +0100

    chore: fix coding style for http authorization header

--- src/Headers/HttpAuthorizationHeader.php
+++ src/Headers/HttpAuthorizationHeader.php
@@ -4,25 +4,20 @@ declare(strict_types=1);
 
 namespace WrkFlow\ApiSdkBuilder\Headers;
 
+use SensitiveParameter;
 use WrkFlow\ApiSdkBuilder\Interfaces\HeadersInterface;
 
 class HttpAuthorizationHeader implements HeadersInterface
 {
     public function __construct(
-        #[\SensitiveParameter] public readonly string $username,
-        #[\SensitiveParameter] public readonly string $password,
+        #[SensitiveParameter] public readonly string $username,
+        #[SensitiveParameter] public readonly string $password,
     ) {
     }
 
     public function headers(): array
     {
-        $key = base64_encode(
-            sprintf(
-                '%s:%s',
-                $this->username,
-                $this->password,
-            ),
-        );
+        $key = base64_encode(sprintf('%s:%s', $this->username, $this->password));
 
         return [
             'Authorization' => sprintf('Basic %s', $key),
